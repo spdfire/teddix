d5bbd6bc1908a75238fcf7fc92150d26b9467900
spdfire <spdfire@plusinfinity.org>
2013-09-09 21:34:02 +0200

    debian pkg template

---
+++ pkg-src/debian/menu.ex
@@ -0,0 +1,2 @@
+?package(teddix):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="teddix" command="/usr/bin/teddix"
---
+++ pkg-src/debian/teddix.default.ex
@@ -0,0 +1,10 @@
+# Defaults for teddix initscript
+# sourced by /etc/init.d/teddix
+# installed at /etc/default/teddix by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
