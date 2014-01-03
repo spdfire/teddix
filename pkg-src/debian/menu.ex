c2275d86dd33e19dbf1a8d990cc20a7512041add
spdfire <spdfire@plusinfinity.org>
2014-01-03 13:48:44 +0100

    removed debian build files

--- pkg-src/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(teddix):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="teddix" command="/usr/bin/teddix"
--- pkg-src/debian/teddix.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the teddix package
-#
-0 4	* * *	root	[ -x /usr/bin/teddix_maintenance ] && /usr/bin/teddix_maintenance
